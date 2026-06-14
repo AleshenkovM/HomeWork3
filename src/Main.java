1932bba81750976974783cc093c354d80b8a4e85
Михаил <aleshenkov29@gmail.com>
2026-06-14 18:56:44 +0500

    Homework2

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       // Задача номер 1
+        byte aplle = 126;
+        short deposit = 32766;
+        int salary = 2_147_483_646;
+        long namber = 9_223_32_036_854_775_806L;
+        float pi = 3.14f;
+        double
+
+
+
+    }
+}
